88ee1828fde02c24df4ebc0b8ff4b2c8e5aff255
Eduardo Flores <efloresambrosio@gmail.com>
2025-07-19 00:40:05 -0600

    Refactors task management to use ULID IDs
    Updates task IDs to use ULID for better uniqueness and scalability.
    
    Changes the task store to use a map instead of a list to allow
    access tasks by ID.

--- in-mem-task-mngr/src/todo/core.clj
+++ in-mem-task-mngr/src/todo/core.clj
@@ -1,7 +1,11 @@
 (ns todo.core)
 
-(def tasks (atom []))
+(require '[clj-ulid :as ulid])
 
+; definiendo la variable tasks como un atom de un mapa
+(def tasks (atom {}))
+
+; definiendo la tarea
 {
  :id 1                                                      ; id tarea
  :description "practice clojure"
@@ -11,13 +15,22 @@
  }
 
 
+; definiendo la funcion generate-id
 (defn generate-id
-  "Generate the next id for the task"
+  "Generate the next id for the task using ULID instead of integer or UUID"
   []
-  (inc (or (-> @tasks last :id) 0)))
+  (ulid/ulid))
 
+(defn add-task
+  "Add a new task to the tasks atom"
+  [task]
+  (swap! tasks assoc (generate-id) task))
+
+; definiendo la funcion generate-id
 (comment
   (println :true)
-  (inc 1)
-  (generate-id)
+  (print (generate-id))
+  (add-task {:description "practice clojure" :status :pending :priority :high :dude-date "2025-07-10"})
+  @tasks
+
   )
